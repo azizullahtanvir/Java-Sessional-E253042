c48fe37d199e4cc07d4098eee57c35076696407a
Aziz Ullah Tanvir <azizullahtanvir9@gmail.com>
2026-06-30 20:13:07 +0600

    Add files via upload

---
+++ Lab-1/TanvirInfo.java
@@ -0,0 +1,20 @@
+public class TanvirInfo {
+
+    public static void main(String[] args) {
+
+        String name = "Tanvir";
+        String studentId = "E253042";
+        int age = 23;
+        String email = "tanvir@example.com";
+        String phone = "0123456789";
+        String address = "Chattogram, Bangladesh";
+
+        System.out.println("Tanvir's Information");
+        System.out.println("Name: " + name);
+        System.out.println("Student ID: " + studentId);
+        System.out.println("Age: " + age);
+        System.out.println("Email: " + email);
+        System.out.println("Phone: " + phone);
+        System.out.println("Address: " + address);
+    }
+}
